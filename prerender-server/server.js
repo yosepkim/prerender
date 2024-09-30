a7cfe098cb2f468a38621d637173388bad470bd9
root <yosepk@gmail.com>
2024-09-30 14:53:44 +0000

    updates the file

--- prerender-server/server.js
+++ prerender-server/server.js
@@ -1,5 +1,6 @@
 const prerender = require('prerender');
 const server = prerender({
+    waitAfterLastRequest: 10000,
     chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
   });
 server.start();
