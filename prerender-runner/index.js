8f5f0996afe0a044cfbe1ec2412a3add9511c47c
Yosep Kim <yosepk@gmail.com>
2024-09-30 12:01:41 -0400

    increases maxBuffer

--- prerender-runner/index.js
+++ prerender-runner/index.js
@@ -50,7 +50,7 @@ function updateHdb(id, content) {
 async function prerender(id, targetUrl) {
   const location = getLocation(targetUrl);
   
-  const { stdout, stderr } = await exec(`curl http://localhost:3000/render?url=${targetUrl}`);
+  const { stdout, stderr } = await exec(`curl http://localhost:3000/render?url=${targetUrl}`, {maxBuffer: 1024 * 500});
   const cleaned = stdout.replaceAll('href="/', `href="${location.protocol}://${location.host}/`)
   updateHdb(id, cleaned);
 
