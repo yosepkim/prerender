83009716aa33742c8833cbceae0a660562c6bfbc
Yosep Kim <yosepk@gmail.com>
2024-10-01 10:57:03 -0400

    adds crawler class

--- web-crawler/crawler.rb
+++ web-crawler/crawler.rb
@@ -1,18 +1,30 @@
 require 'watir'
 require 'headless'
 
-browser = Watir::Browser.new :chrome, headless: true
 
-def process_link(hostname, target_link, browser)
-    if target_link.kind_of?(String) && target_link.start_with?(hostname)
-        browser.goto target_link
-        browser.links.each do |link|
-            `node ../prerender-runner/index.js #{target_link}`
-            sleep 10
-            process_link(hostname, link, browser)
+
+class Crawler
+
+    @@processed = []
+
+    def process_link(hostname, target_link, browser)
+        if target_link.kind_of?(String) && !@@processed.include?(target_link) && target_link.start_with?(hostname)
+            browser.goto(target_link)
+            @@procssed.push(target_link)
+            browser.links.each do |link|
+                `node ../prerender-runner/index.js #{target_link}`
+                sleep 10
+                process_link(hostname, link, browser)
+            end
         end
     end
+
 end
 
+browser = Watir::Browser.new :chrome, headless: true
+
+crawler = Crawler.new
+
+
 #process_link('https://www.brooksrunning.com', 'https://www.brooksrunning.com/en_us/sitemap/', browser)
-process_link('https://cars.edgecloud9.com', 'https://cars.edgecloud9.com/', browser)
+crawler.process_link('https://cars.edgecloud9.com', 'https://cars.edgecloud9.com/', browser)
