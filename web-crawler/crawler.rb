ff96ba0e194c1eb78e055ab9151605ea544c4ee1
Yosep Kim <yosepk@gmail.com>
2024-10-01 10:44:36 -0400

    fixes typo

--- web-crawler/crawler.rb
+++ web-crawler/crawler.rb
@@ -5,11 +5,12 @@ browser = Watir::Browser.new :chrome, headless: true
 
 def process_link(hostname, target_link, browser)
     if target_link.starts_with?(hostname)
-    browser.goto target_link
-    browser.links.each do |link|
-        `node ../prerender-runner/index.js #{target_link}`
-        sleep 10
-        process_link(hostname, link, browser)
+        browser.goto target_link
+        browser.links.each do |link|
+            `node ../prerender-runner/index.js #{target_link}`
+            sleep 10
+            process_link(hostname, link, browser)
+        end
     end
 end
 
