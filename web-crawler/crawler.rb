0815689702b14e01a00aa2aaf023f4847aaee62a
Yosep Kim <yosepk@gmail.com>
2024-10-01 10:43:15 -0400

    delays each call

--- web-crawler/crawler.rb
+++ web-crawler/crawler.rb
@@ -3,13 +3,14 @@ require 'headless'
 
 browser = Watir::Browser.new :chrome, headless: true
 
-def process_link(target_link, browser)
+def process_link(hostname, target_link, browser)
+    if target_link.starts_with?(hostname)
     browser.goto target_link
     browser.links.each do |link|
         `node ../prerender-runner/index.js #{target_link}`
-        process_link(link, browser)
         sleep 10
+        process_link(hostname, link, browser)
     end
 end
 
-process_link('https://www.brooksrunning.com/en_us/sitemap/', browser)
+process_link('https://www.brooksrunning.com', 'https://www.brooksrunning.com/en_us/sitemap/', browser)
