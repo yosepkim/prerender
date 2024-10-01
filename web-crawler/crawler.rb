9a33b544d34fac97d6004140a01b7becfbacd6fa
Yosep Kim <yosepk@gmail.com>
2024-10-01 10:47:24 -0400

    points to another domain

--- web-crawler/crawler.rb
+++ web-crawler/crawler.rb
@@ -14,4 +14,5 @@ def process_link(hostname, target_link, browser)
     end
 end
 
-process_link('https://www.brooksrunning.com', 'https://www.brooksrunning.com/en_us/sitemap/', browser)
+#process_link('https://www.brooksrunning.com', 'https://www.brooksrunning.com/en_us/sitemap/', browser)
+process_link('https://cars.edgecloud9.com', 'https://www.edgecloud9.com/', browser)
