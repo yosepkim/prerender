ad6cc3a090e95d85aa8f4ac392ac7a52f44af0da
Yosep Kim <yosepk@gmail.com>
2024-10-01 15:55:43 -0400

    cleans up code

--- web-crawler/crawler.rb
+++ web-crawler/crawler.rb
@@ -9,16 +9,16 @@ class Crawler
     def process_link(hostname, target_link, browser)
         target_link = target_link.split('#')[0]
         puts "processing #{target_link}"
-        if target_link.kind_of?(String) &&
+        if (target_link.kind_of?(String) &&
             !@@processed.include?(target_link) &&
             target_link.start_with?(hostname) &&
-            !@@skip.any? { |substring| target_link.include?(substring) } 
+            !@@skip.any? { |substring| target_link.include?(substring) })
             
             browser.goto(target_link)
 
             begin
                 `node ../prerender-runner/index.js #{target_link} &`
-                sleep 10
+                sleep 5
             rescue => error
                 puts "Error: #{error.message}"
             end
